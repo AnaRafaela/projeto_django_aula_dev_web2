faa31127e709170ecf442dcc92d91711eb7fe15f
AnaRafaela <anarafaela30103@gmail.com>
2019-08-15 13:50:34 -0300

    finalizado template login

--- app/models.py
+++ app/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from ckeditor.fields import RichTextField
-from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    resumo = RichTextField()
-    content = RichTextUploadingField()
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    resumo = models.TextField()
+    imagem = models.ImageField(upload_to='imagens', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -11,15 +11,16 @@ class PostList(View):
     template = 'post_list.html'
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, self.template)
+        return render(request, self.template, {'posts':posts})
 
 class PostCreate(CreateView):
     model = Post
-    fields = ['title', 'resumo', 'content', 'author']
+    fields = ['title', 'resumo', 'imagem']
+    success_url = reverse_lazy('main')
 
 class PostUpdate(UpdateView):
     model = Post
-    fields = ['title', 'resumo', 'content', 'author']
+    fields = ['title', 'resumo', 'imagem']
 
 class PostDelete(DeleteView):
     model = Post
